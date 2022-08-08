35813486fb4996e149f91fde59eaeca3ff4b1216
Mike Pro <mike@lucenka.org>
2022-08-08 16:21:59 +0200

    code style

--- src/latex.py
+++ src/latex.py
@@ -1,97 +1,140 @@
-import shutil
-
-from pylatex import Document, Section, Subsection, Tabular, Math, TikZ, Axis, \
-    Plot, Figure, Matrix, Alignat, Command, UnsafeCommand
-from pylatex.base_classes import CommandBase, Arguments, Environment
-from pylatex.utils import NoEscape
-import exp
 import os
+import shutil
 from datetime import datetime
+
+from pylatex import (
+    Alignat,
+    Axis,
+    Command,
+    Document,
+    Figure,
+    Math,
+    Matrix,
+    Plot,
+    Section,
+    Subsection,
+    Tabular,
+    TikZ,
+    UnsafeCommand,
+)
+from pylatex.base_classes import Arguments, CommandBase, Environment
+from pylatex.utils import NoEscape
+
+import exp
 
 
 class TwentyItem(CommandBase):
-    _latex_name = 'twentyitem'
+    _latex_name = "twentyitem"
 
 
 class Twenty(Environment):
-    _latex_name = 'twenty'
+    _latex_name = "twenty"
 
 
-if __name__ == '__main__':
-    image_filename = os.path.join(os.path.dirname(__file__), 'img/linkedin.png')
+if __name__ == "__main__":
+    image_filename = os.path.join(os.path.dirname(__file__), "img/linkedin.png")
 
-    name = 'Michael'
-    surname = 'Prokopenko'
-    title = 'DevOps'
+    name = "Michael"
+    surname = "Prokopenko"
+    title = "DevOps"
     doc = Document(
-        documentclass='twentysecondcv',
-        document_options=["a4paper", ],
+        documentclass="twentysecondcv",
+        document_options=[
+            "a4paper",
+        ],
         fontenc=None,
     )
-    doc.preamble.append(Command('cvname', NoEscape(f'{name}\\\\{surname}')))  # your name
-    doc.preamble.append(Command('cvjobtitle', title))  # your actual job position
-    doc.preamble.append(Command('cvaddress', 'Montenegro, Bar'))  # address
-    doc.preamble.append(Command('cvnumberphone', '+382 6853 9017'))  # telephone number
-    doc.preamble.append(Command('cvmail', 'mike.pro@alexsupport.org'))  # e-mail
-    doc.preamble.append(Command('cvlinkedin', 'linkedin.com/in/ligzer'))
-    doc.preamble.append(Command('cvtelergram', 't.me/ligzer'))
-    doc.preamble.append(Command('cvrelocation', 'Relocation possible'))
-    doc.preamble.append(NoEscape('\\def\changemargin#1#2{\\list{}{\\rightmargin#2\\leftmargin#1}\\item[]}'
-                                 '\\let\endchangemargin=\\endlist'))
-    doc.preamble.append(Command(
-        'skills',
-        # \\textbf{Vagrant}
-        arguments=[
+    doc.preamble.append(
+        Command("cvname", NoEscape(f"{name}\\\\{surname}"))
+    )  # your name
+    doc.preamble.append(Command("cvjobtitle", title))  # your actual job position
+    doc.preamble.append(Command("cvaddress", "Montenegro, Bar"))  # address
+    # https://www.google.com/maps/place/Bar,+Montenegro/@42,19,6z/
+    doc.preamble.append(Command("cvnumberphone", "+382 6853 9017"))  # telephone number
+    doc.preamble.append(Command("cvmail", "mike.pro@alexsupport.org"))  # e-mail
+    doc.preamble.append(Command("cvlinkedin", "linkedin.com/in/ligzer"))
+    doc.preamble.append(Command("cvtelergram", "t.me/ligzer"))
+    doc.preamble.append(Command("cvrelocation", "Relocation possible"))
+    doc.preamble.append(
+        NoEscape(
+            "\\def\changemargin#1#2{\\list{}{\\rightmargin#2\\leftmargin#1}\\item[]}"
+            "\\let\endchangemargin=\\endlist"
+        )
+    )
+    doc.preamble.append(
+        Command(
+            "skills",
+            # \\textbf{Vagrant}
+            arguments=[
+                NoEscape(
+                    "\\textbf{DevOps},"
+                    "\\textbf{~Proxmox~},"
+                    "\\textbf{Sentry},"
+                    "\\textbf{Zabbix},"
+                    "\\textbf{GitLab CI},"
+                    "\\textbf{Ansible},"
+                    "\\textbf{Terraform},"
+                    "\\textbf{Docker},"
+                    "\\textbf{~~k8s~~},"
+                    "\\textbf{Systemd},"
+                    "\\textbf{~~~~Esxi~~~~}"
+                ),
+                NoEscape(
+                    "\\textbf{Dev},"
+                    "\\textbf{~~~~~Python~~~~~},"
+                    "\\textbf{~~~C++~~~},"
+                    "\\textbf{~~~~~Django~~~~~},"
+                    "\\textbf{~~~~Celery~~~~},"
+                    "\\tiny\\textbf{javascript},"
+                    "\\textbf{React},"
+                    "\\textbf{Redux}"
+                ),
+                NoEscape(
+                    " \\textbf{other},"
+                    "\\textbf{~~~~~Linux~~~~~},"
+                    "\\textbf{Win Servers},"
+                    "\\scriptsize\\textbf{Active}\\\\\scriptsize\\textbf{Directory},"
+                    "\\textbf{Mikrotik},"
+                    "\\textbf{Asterisk},"
+                    "\\textbf{Networking}"
+                ),
+            ],
+        )
+    )
 
-            NoEscape(
-                '\\textbf{DevOps}, \\textbf{GitLab CI},\\textbf{Ansible},\\textbf{~Proxmox~},'
-                '\\textbf{~~~~Esxi~~~~},\\textbf{Sentry},\\textbf{Systemd},\\textbf{Docker},\\textbf{Zabbix}'
-            ),
-            NoEscape(
-                '\\textbf{Dev},'
-                '\\textbf{~~~~~Python~~~~~},'
-                '\\textbf{~~~C++~~~},'
-                '\\textbf{~~~~~Django~~~~~},'
-                '\\textbf{~~~~Celery~~~~},'
-                '\\tiny\\textbf{javascript},'
-                '\\textbf{React},'
-                '\\textbf{Redux}'),
-            NoEscape(
-                ' \\textbf{other},'
-                '\\textbf{~~~~~Linux~~~~~},'
-                '\\textbf{Win Servers},'
-                '\\scriptsize\\textbf{Active}\\\\\scriptsize\\textbf{Directory},'
-                '\\textbf{Mikrotik},'
-                '\\textbf{Asterisk},'
-                '\\textbf{Networking}'),
-        ]
-    ))
-
-    doc.append(Command('makeprofile'))
-    doc.append(NoEscape('{\Huge\headingfont\color{headercolor}\givencvjobtitle}'))
-    with doc.create(Section('Work Experience')):
+    doc.append(Command("makeprofile"))
+    doc.append(NoEscape("{\Huge\headingfont\color{headercolor}\givencvjobtitle}"))
+    with doc.create(Section("Work Experience")):
         with doc.create(Twenty()):
             for job in [x for x in exp.jobs if not x.hide][0:6]:
                 job: exp.Job = job
-                doc.append(TwentyItem(arguments=Arguments(
-                    NoEscape(job.dates()),
-                    NoEscape(job.title),
-                    NoEscape(job.company),
-                    NoEscape(job.format),
-                    job.description
-                )))
+                doc.append(
+                    TwentyItem(
+                        arguments=Arguments(
+                            NoEscape(job.dates()),
+                            NoEscape(job.title),
+                            NoEscape(job.company),
+                            NoEscape(job.format),
+                            job.description,
+                        )
+                    )
+                )
 
-    with doc.create(Section('Education')):
+    with doc.create(Section("Education")):
         with doc.create(Twenty()):
             for edu in exp.educations:
                 edu: exp.Job = edu
-                doc.append(TwentyItem(arguments=Arguments(
-                    NoEscape(edu.dates()),
-                    NoEscape(edu.title),
-                    NoEscape(edu.company),
-                    NoEscape(edu.format),
-                    NoEscape(edu.description)
-                )))
-    pdfname = f'./latex/{title} {name} {surname}'
+                doc.append(
+                    TwentyItem(
+                        arguments=Arguments(
+                            NoEscape(edu.dates()),
+                            NoEscape(edu.title),
+                            NoEscape(edu.company),
+                            NoEscape(edu.format),
+                            NoEscape(edu.description),
+                        )
+                    )
+                )
+    pdfname = f"./latex/{title} {name} {surname}"
     doc.generate_pdf(pdfname, clean_tex=False, compiler="xelatex")
-    shutil.copy(f"{pdfname}.pdf", '..')
+    shutil.copy(f"{pdfname}.pdf", "..")
--- src/main.py
+++ src/main.py
@@ -1,30 +1,54 @@
 from datetime import date, timedelta
-import matplotlib.pyplot as plt
 from itertools import cycle
 
-cycol = cycle('bgrcmk')
-cyrange = cycle(range(1, 100, ))
+import matplotlib.pyplot as plt
+
+cycol = cycle("bgrcmk")
+cyrange = cycle(
+    range(
+        1,
+        100,
+    )
+)
 
 
 def add_job(job):
-    x = [job.begin, job.end, ]
+    x = [
+        job.begin,
+        job.end,
+    ]
     # corresponding y axis values
     i = next(cyrange)
     y = [i, i]
 
     # plotting the points
-    plt.plot(x, y, color=next(cycol), linewidth=2,
-             markerfacecolor='blue', marker='.', markersize=6)
+    plt.plot(
+        x,
+        y,
+        color=next(cycol),
+        linewidth=2,
+        markerfacecolor="blue",
+        marker=".",
+        markersize=6,
+    )
     plt.text(job.begin, i, job.title)
 
-    print("\\twentyitem\n"
-          f"{{{job.begin.strftime('%b %Y')} - \\\\ {job.end.strftime('%b %Y')}}}\n"
-          f"{{ {job.title} }}\n")
+    print(
+        "\\twentyitem\n"
+        f"{{{job.begin.strftime('%b %Y')} - \\\\ {job.end.strftime('%b %Y')}}}\n"
+        f"{{ {job.title} }}\n"
+    )
     # print(f"{job.begin.strftime('%b %Y')} - {job.end.strftime('%b %Y')}  {job.title}")
 
 
 class Job:
-    def __init__(self, begin, title='', end=date.today(), len=None, ):
+    def __init__(
+        self,
+        begin,
+        title="",
+        end=date.today(),
+        len=None,
+    ):
         self.begin = begin
         if len:
             self.end = self.begin + timedelta(days=30 * len)
@@ -43,18 +67,26 @@ class Job:
 
 
 jobs = [
-    Job(date(2008, 1, 1), 'BMSTU', date(2011, 5, 1), ),
-    Job(date(2011, 3, 1), 'Mallina LTD - IT Support', date(2013, 6, 1)),
-    Job(date(2011, 6, 1), 'Mallina LTD - System and Database administrator', date(2013, 6, 1)),
-    Job(date(2013, 6, 1), 'Individual Entrepreneur Dmitriev - IT Support'),
-    Job(date(2011, 3, 1), 'planus - pbx System Developer(Python)', len=36),
-    Job(date(2018, 1, 1), 'pycards - Developer(Python, C++)', len=12),
-    Job(date(2018, 9, 1), 'planus - telegram resend Developer(Python)', len=12),
-    Job(date(2014, 1, 1), 'planus - sync tickets System Developer(Python)', len=24),
-    Job(date(2019, 12, 1), 'telegram reserve bot Developer(Python)', len=12),
-    Job(date(2017, 11, 2), 'anzu sklad - Fullstack developer(JS, Python)', len=24),
-    Job(date(2021, 1, 1), 'DMDK anzu sklad - Fullstack developer(JS, Python)'),
-    Job(date(2022, 3, 1), 'YourCodeReview (github administrator)'),
+    Job(
+        date(2008, 1, 1),
+        "BMSTU",
+        date(2011, 5, 1),
+    ),
+    Job(date(2011, 3, 1), "Mallina LTD - IT Support", date(2013, 6, 1)),
+    Job(
+        date(2011, 6, 1),
+        "Mallina LTD - System and Database administrator",
+        date(2013, 6, 1),
+    ),
+    Job(date(2013, 6, 1), "Individual Entrepreneur Dmitriev - IT Support"),
+    Job(date(2011, 3, 1), "planus - pbx System Developer(Python)", len=36),
+    Job(date(2018, 1, 1), "pycards - Developer(Python, C++)", len=12),
+    Job(date(2018, 9, 1), "planus - telegram resend Developer(Python)", len=12),
+    Job(date(2014, 1, 1), "planus - sync tickets System Developer(Python)", len=24),
+    Job(date(2019, 12, 1), "telegram reserve bot Developer(Python)", len=12),
+    Job(date(2017, 11, 2), "anzu sklad - Fullstack developer(JS, Python)", len=24),
+    Job(date(2021, 1, 1), "DMDK anzu sklad - Fullstack developer(JS, Python)"),
+    Job(date(2022, 3, 1), "YourCodeReview (github administrator)"),
 ]
 
 
@@ -68,5 +100,5 @@ def print_years():
     plt.show()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print_years()
